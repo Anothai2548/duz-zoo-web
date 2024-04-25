aeaffb6af2febd67c1c1b61cf88e26eb0a141772
Anothai <anothai2305@gmail.com>
2024-04-25 13:26:15 +0700

    duz-web

---
+++ duz-web/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzQk31jgQqlLu0mqN8s5nDg5TDRrwhVIakyS29f8DZPQ&s" className="App-logo" alt="logo" />
+      </header>
+      <div>
+      <ul class='navbar-nav'>
+          <li class='nav-item' className='item'><a>Home</a></li>
+          <li class='nav-item' className='item'><a>Home</a></li>
+          <li class='nav-item' className='item'><a>Home</a></li>
+          <li class='nav-item' className='item'><a>Home</a></li>
+        </ul>
+      </div>
+
+      {/* components zone */}
+    </div>
+  );
+}
+
+export default App;
